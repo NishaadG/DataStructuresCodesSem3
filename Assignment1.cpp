c2e5951559dcf6e6d5696943aa3fe65bdce21d24
NishaadG <149945175+NishaadG@users.noreply.github.com>
2024-08-20 21:57:50 -0700

    Update Assignment1.cpp
    Final assignment1 without exception handling

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -32,7 +32,7 @@ Node* newRegister(int id,string Name , int priority){
 Node* addAtEnd(Node* head , Node *  newNode){
     if(head==NULL) return newNode;
     Node* temp = head;
-    while(temp!=NULL){
+    while(temp->next!=NULL){
         temp=temp->next;
     }
     temp->next=newNode;
@@ -70,12 +70,52 @@ void changeData(Node* head , int changechoice , int data,string Namechoice){
         temp->priority=data;
     }
 }
+
+Node* removePatient( Node* head , string nametbr){
+    if(head->name==nametbr){
+        return head->next;
+    }
+    Node* temp = head;
+    Node* prev = NULL;
+    while(temp!=NULL){
+        if(nametbr==temp->name){
+            break;
+        }
+        prev=temp;
+        temp=temp->next;
+    }
+    if(temp->name==nametbr){
+        prev->next=temp->next;
+        return head;
+    }
+    else{
+        cout<<"Name not found."<<endl;
+        return head;
+    }
+}
+
+void searchPatient(Node* head , string nametbf){
+    Node* temp = head ;
+    while(temp!=NULL){
+        if(temp->name==nametbf){
+            cout<<"Name " << nametbf <<" has been found" <<endl;
+            cout<<"ID of patient  : "<<temp->id<<endl;
+            cout<<"Name of patient : "<<temp->name<<endl;
+            cout<<"Priority of patient : "<<temp->priority<<endl;
+            return;
+        }
+        temp=temp->next;
+    }
+    cout<<"Name not found."<<endl;
+    return ;
+}
 int main(){
     int choice;
     Node* head=NULL;
     do{
         DisplayMenu();
         cin>>choice;
+        if(choice>5||choice<0) cout<< "Invalid input choose again ."<<endl;;
         if(choice==1){
             string Name;
             int priority;
@@ -93,7 +133,7 @@ int main(){
             if(position==1){
                 head = addAtStart(head , newNode);
             }
-            else if(position==2){
+            else if(position==3){
                 head = addAtEnd(head , newNode);
             }
         }
@@ -110,6 +150,21 @@ int main(){
             cin>>data;
             changeData(head , changechoice , data , tbfname);
             printInfo(head);
+        }
+        else if(choice==3){
+            printInfo(head);
+            string tbrname;
+            cout<<"Enter Name of patient who is to be removed : " << endl;
+            cin>>tbrname;
+            head =removePatient(head , tbrname);
+            cout<<"New Queue : " <<endl;
+            printInfo(head);
+        }
+        else if(choice==4){
+            string nametbf ;
+            cout<<"Enter Name of patient to be found : " <<endl;
+            cin>>nametbf;
+            searchPatient(head , nametbf);
         }
     } while(choice!=5);
 }
