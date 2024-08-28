bd86ca3e09e43d51a5c3cfacf3127949fb55a8f0
NishaadG <149945175+NishaadG@users.noreply.github.com>
2024-08-27 22:25:11 -0700

    Update Assignment4.cpp

--- Assignment4.cpp
+++ Assignment4.cpp
@@ -38,18 +38,8 @@ pair<int , int> highestAverage(vector <vector<int >> arr ,int subs,int students 
     return {maxmarks , subno};
 }
 
-int main(){
-    vector <vector <int> > nums = {
-        {0,0,10 , 0},
-        {21 , 0 , 17 ,0},
-        {0,12 , 0 ,0 },
-        {0,0,7,0},
-        {0,0,25,3},
-        {12,0,0,1},
-        {0,0,56,0},
-    };
+vector<vector<int>> convertToCompact(vector<vector<int>>nums){
     vector <vector <int> >efficientArr;
-    printArray(nums);
     int nonzeros = 0;
     for(int i = 0 ;i < nums.size();i++){
         for(int j = 0 ;j<nums[0].size();j++){
@@ -60,14 +50,38 @@ int main(){
             }
         }
     }
+    return efficientArr;
+}
+
+vector <pair<int , int > > highestMarkStudents(vector<vector<int>>nums){
+    vector <pair<int ,int > highestMark;
+    for(int i = 0 ;i < nums.size();i++){
+        if()
+    }
+}
+
+int main(){
+    vector <vector <int> > nums = {
+        {0,0,10 , 0},
+        {21 , 0 , 17 ,0},
+        {0,12 , 0 ,0 },
+        {0,0,7,0},
+        {0,0,25,3},
+        {12,0,0,1},
+        {0,0,56,0},
+    };
+    int students = nums.size();
+    int subjects = nums[0].size();
+    vector <vector <int> >efficientArr;
+    efficientArr = convertToCompact(nums);
     
     
     
     cout<<"Efficient Array : " <<endl;
     printArray(efficientArr);
     cout<<"avg marks : " <<endl;
-    printArray(findAverage(efficientArr , nums[0].size(),nums.size()));
+    printArray(findAverage(efficientArr , subjects,students));
     pair < int , int > highest ;
-    highest = highestAverage(efficientArr , nums[0].size(),nums.size());
+    highest = highestAverage(efficientArr , subjects,students);
     cout<<"maximum average : "<<highest.first <<" Subject : "<<highest.second;
 }
