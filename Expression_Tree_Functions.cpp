b7d58d62d8824e1f09860bc2317fd3caceeb5e5f
NishaadG <149945175+NishaadG@users.noreply.github.com>
2024-11-05 18:37:10 +0530

    Update Expression_Tree_Functions.cpp

--- Expression_Tree_Functions.cpp
+++ Expression_Tree_Functions.cpp
@@ -1,57 +1,71 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Node{
-    public:
+class Node {
+public:
     Node* left;
     Node* right;
     int data;
     
-    Node(char data){
+    Node(char data) {
         this->data = data;
-        this->left =NULL;
-        this->right =NULL;
+        this->left = NULL;
+        this->right = NULL;
     }
 };
 
-Node* postfixToBST(string str){
+Node* postfixToBST(string str) {
     stack<Node*> st;
     Node* newNode;
-    for(int i= 0;i < str.size();i++){
-        if (str[i] == '+' || str[i] == '-' || str[i] == '*'
-            || str[i] == '/' || str[i] == '^'){
+    for (int i = 0; i < str.size(); i++) {
+        if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/' || str[i] == '^') {
             newNode = new Node(str[i]);
-            Node* left = st.top();
-            st.pop();
             Node* right = st.top();
+            st.pop();
+            Node* left = st.top();
             st.pop();
             newNode->left = left;
             newNode->right = right;
             st.push(newNode);
-        }
-        else{
+        } else {
             newNode = new Node(str[i]);
             st.push(newNode); 
         }
     }
-    return newNode;
+    return st.top();
 }
 
-Node* prefixToBST(string str){
-    reverse(str.begin(),str.end());
+Node* prefixToBST(string str) {
+    reverse(str.begin(), str.end());
     return postfixToBST(str);
 }
-void inOrder(Node* root){
-    if(root==NULL) return ;
+
+void inOrder(Node* root) {
+    if (root == NULL) return;
     inOrder(root->left);
-    cout<<(char)root->data<<" ";
+    cout << (char)root->data << " ";
     inOrder(root->right);
 }
 
-int main(){
-    Node* root = postfixToBST("DC-BA+*");
-    Node* root2 = prefixToBST("*+AB-CD");
+int main() {
+    string postfixExpr, prefixExpr;
+    
+    cout << "Enter postfix expression: ";
+    cin >> postfixExpr;
+    
+    cout << "Enter prefix expression: ";
+    cin >> prefixExpr;
+    
+    Node* root = postfixToBST(postfixExpr);
+    Node* root2 = prefixToBST(prefixExpr);
+    
+    cout << "Inorder traversal of BST from postfix expression: ";
     inOrder(root);
-    cout<<endl;
+    cout << endl;
+    
+    cout << "Inorder traversal of BST from prefix expression: ";
     inOrder(root2);
+    cout << endl;
+
+    return 0;
 }
